ccfe49893d755bbaa5c12c1b1a81e9cecbe05cf4
sumaiya-rahman-chowdhury <bdfara@1>
2024-10-27 11:00:20 -0700

    make our server respond to different requests differently

--- server_create/server_create.js
+++ server_create/server_create.js
@@ -1,8 +1,20 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.end('Hello World')
+    if(req.url === '/'){
+        res.end('This is my Home Page')
+    }
+    else if(req.url === '/about'){
+        res.end('This is about Page')
+    }
+    else if(req.url === '/contact'){
+        res.end('This is Contact Page')
+    }
+    else{
+        res.end('404, Resource Not Found'); 
+    }
 })
-server.listen(5000,()=>{
+
+server.listen(5001,()=>{
     console.log("Server is running")
 })
